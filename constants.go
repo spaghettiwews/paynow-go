f828ec81f4f3374aa44b189d2c093aaf4cab5545
spaghettiwews <spaghettiwews@outlook.com>
2019-11-24 18:24:40 +0200

    add paynow api constants

---
+++ constants.go
@@ -0,0 +1,9 @@
+package paynow-go
+
+const (
+	ResponseOk string = "ok"
+	ResponseError string = "error"
+	ResponseInvalidId string = "invalid id"
+	UrlInitiateTransaction string = "https://www.paynow.co.zw/interface/initiatetransaction"
+	UrlInitiateMobileTransaction string = "https://www.paynow.co.zw/interface/initiatetransaction"
+)
