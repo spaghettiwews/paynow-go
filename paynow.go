993ef3028be59ec08f7b1f8fcdf00ae565410969
spaghettiwews <spaghettiwews@outlook.com>
2019-11-24 18:27:48 +0200

    add Paynow custom type

---
+++ paynow.go
@@ -0,0 +1,15 @@
+package paynow-go
+
+type Paynow struct {
+	id, key, resultUrl, returnUrl string
+	Payment Payment
+}
+
+func NewPaynow(id string, key string, resultUrl string, returnUrl string) *Paynow {
+    p := new(Paynow)
+    p.id = id
+    p.key = key
+    p.resultUrl = resultUrl
+    p.returnUrl = returnUrl
+    return p
+}
